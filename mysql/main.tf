44f81f6c63216dfc612de8831dd80a297686617a
Naga Srinivas <59291856+srinivas325@users.noreply.github.com>
2021-08-10 18:29:15 +0100

    Update main.tf

--- mysql/main.tf
+++ mysql/main.tf
@@ -5,31 +5,20 @@
 # Note: This code is meant solely as a simple demonstration of how to lay out your files and folders with Terragrunt
 # in a way that keeps your Terraform code DRY. This is not production-ready code, so use at your own risk.
 # ---------------------------------------------------------------------------------------------------------------------
+
 terraform {
-  required_providers {
+  # Live modules pin exact Terraform version; generic modules let consumers pin the version.
+  # The latest version of Terragrunt (v0.29.0 and above) recommends Terraform 0.15.0 or above.
+ required_version = ">= 0.13.0"
+
+  # Live modules pin exact provider version; generic modules let consumers pin the version.
+required_providers {
     aws = {
-      source = "hashicorp/aws"
-      version = "3.53.0"
+     source  = "hashicorp/aws"
+      version = ">= 3.0.0"
     }
   }
 }
-
-provider "aws" {
-  # Configuration options
-}
-#terraform {
-  # Live modules pin exact Terraform version; generic modules let consumers pin the version.
-  # The latest version of Terragrunt (v0.29.0 and above) recommends Terraform 0.15.0 or above.
-#  required_version = ">= 1.0.0"
-
-  # Live modules pin exact provider version; generic modules let consumers pin the version.
-#  required_providers {
-   # aws = {
-  #    source  = "hashicorp/aws"
-    #  version = "= 3.53.0"
-   # }
-  #}
-#}
 
 # ---------------------------------------------------------------------------------------------------------------------
 # CREATE THE MYSQL DB
